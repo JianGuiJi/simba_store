e44ffbd029d205ab87942ed2e2370bcc93793c30
jijg <jijg@lizi.com>
2018-03-08 13:27:54 +0800

    暂存
    暂存表结构设计

--- database/migrations/2018_03_08_130022_create_stock_logs_table.php
+++ database/migrations/2018_03_08_130022_create_stock_logs_table.php
@@ -15,6 +15,11 @@ class CreateStockLogsTable extends Migration
     {
         Schema::create('stock_logs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string("stock_id", 64)->comment('配货单号');
+            $table->string('status',16)->comment('状态');
+            $table->string('content')->comment('日志内容');
+            $table->string('com_id', 32)->comment('公司编码');
+            $table->string('user_id',32)->comment('用户uuid');
             $table->timestamps();
         });
     }
